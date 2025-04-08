0db20f405738d3e81871591f8bff3da49f6f2e93
Yash Srivastava <console22sat@gmail.com>
2025-04-08 18:25:48 +0000

    added BOT_TOKEN

--- script.py
+++ script.py
@@ -1,6 +1,10 @@
+import os
+
 import requests
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
 
 # Function to get a random quote
 def get_quote():
@@ -19,7 +23,7 @@ async def quote(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 # Main function to start the bot
 if __name__ == "__main__":
-    app = ApplicationBuilder().token("7624300048:AAEG01YrQ2nwvDSD3gCDtskvepD8mEyJqGg").build()
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("quote", quote))
